1b4d204b31e3789859840f4056b1ce841fa2d444
diegofernandodev <dieferv@gmail.com>
2024-04-20 20:14:39 -0500

    se configura archivo .env

--- src/config/DatabaseLittleBox.js
+++ src/config/DatabaseLittleBox.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 // Conexión a la base de datos MongoDB llamada "LittleBox" mongodb://localhost:27017/
 mongoose
 
-  .connect("mongodb+srv://littlebox-conect:42TXTiElhC4RjkpI@littlebox-cluster.rkg9n4v.mongodb.net/", {
+  .connect(process.env.DATABASE_MONGO, {
     // .connect("mongodb://127.0.0.1:27017/LittleBox", {
 
   useUnifiedTopology: true, // Se utiliza para manejar el nuevo motor de topología de MongoDB
